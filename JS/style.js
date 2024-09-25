d24810b9cc76511d53444b1c7402fc12de2768ba
Matthew Digiso <126mdigiso@frhsd.com>
2024-09-25 14:00:15 -0400

    commit and push

---
+++ JS/style.js
@@ -0,0 +1,10 @@
+.splash {
+    background-image: URL("../img/bg.png");
+    background-size: cover;
+background-position: bottom;
+color:#FFF;
+padding-top: 40pageXOffset;
+height: 835pageXOffset;
+}
+
+.btn-outline-SVGFESpotLightElement.active
